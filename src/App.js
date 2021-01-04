6423c82c56a4e0f7fe54b41f8600b1fe755444e6
LeeThomas13 <lmtthomasritz@gmail.com>
2021-01-04 14:03:02 -0800

    made simple react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import main from './Main.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        mainMessage: 'Hola Mundo',
+        extraKey: "extra value",
+    }
+    this.frenchClicked = this.frenchClicked.bind(this);
+  }
+
+  frenchClicked() {
+    this.setState({
+      mainMessage: "Bonjour",
+    });
+  }
+
+  render() {
+      return (
+        <div className="App">
+        <Header />
+        <Main message={this.state.mainMessage} title="lovin it" />
+        <button onClick={this.frenchClicked}>French</button>
+        <Footer description="this is the footers description"/>
+        <Extra text={this.state.mainMessage} />
+        </div>
+      )
+  }
+
+}
+
+function Header(){
+  return(
+    <nav>
+        <h3>Header Coming Soon</h3>
+    </nav>  
+  )
+}
+
+function Main(props){
+  return(
+    <>
+      <h1>React Is Saucy</h1>
+      <h2>{props.title}</h2>
+      <p>{props.message}</p>
+    </>
+  )
+}
+
+function Extra(props){
+  return (props.text)
+}
+
+function Footer(props){
+  return(
+      <footer>
+        <h3>Footer Coming Soon</h3>
+        <p>{props.description}</p>
+      </footer>
+  )
 }
 
 export default App;
