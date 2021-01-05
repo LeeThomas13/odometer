acb28c8f98462b0dfcc25201066b24d630511bd5
LeeThomas13 <lmtthomasritz@gmail.com>
2021-01-05 10:42:28 -0800

    made odometer its own class

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import React from 'react';
+import React, { useImperativeHandle } from 'react';
 import './App.css';
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-        odometer : 0
-    }
-
-  }
-
-  add_miles(miles) {
-    this.setState({
-      odometer : (this.state.odometer + miles) % 10000
-    })
-  }
-
 
   render() {
       return (
         <div className="App">
         <Header title="ODOMETER"/>
-        <h2>{this.state.odometer.toString().padStart(4, "0")}</h2>
-        <h3>Miles</h3>
-        <button onClick={() => this.add_miles(1)}>+1</button>
-        <button onClick={() => this.add_miles(10)}>+10</button>
-        <button onClick={() => this.add_miles(100)}>+100</button>
-        <button onClick={() => this.add_miles(1000)}>+1,000</button>
+        <Odometer />
         <Footer />
         </div>
       )
@@ -40,6 +20,35 @@ function Header(props) {
           <h1>{props.title}</h1>
       </nav>
   )
+}
+
+class Odometer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      odometer : 0
+    }
+  }
+
+  add_miles(miles) {
+    this.setState({
+      odometer : (this.state.odometer + miles) % 10000
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <h2>{this.state.odometer.toString().padStart(4, "0")}</h2>
+        <h3>Miles</h3>
+        <button onClick={() => this.add_miles(1)}>+1</button>
+        <button onClick={() => this.add_miles(10)}>+10</button>
+        <button onClick={() => this.add_miles(100)}>+100</button>
+        <button onClick={() => this.add_miles(1000)}>+1,000</button>
+      </>
+    )
+  }
 }
 
 function Footer(props) {
